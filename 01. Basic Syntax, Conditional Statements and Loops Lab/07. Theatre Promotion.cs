cee51b8dc2113fcfc523ab5f57e6a6c9c0175aa2
blueshero92 <133801686+blueshero92@users.noreply.github.com>
2023-05-31 08:45:39 +0300

    Add files via upload

---
+++ 01. Basic Syntax, Conditional Statements and Loops Lab/07. Theatre Promotion.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace _07._Theatre_Promotion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string day = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            int ticketPrice = 0;
+
+            if(day == "Weekday")
+            {
+                if (age <= 18)
+                {
+                    ticketPrice = 12;
+                }
+                else if (age <= 64)
+                {
+                    ticketPrice = 18;
+                }
+                else
+                {
+                    ticketPrice = 12;
+                }
+            }
+            else if (day == "Weekend")
+            {
+                if (age <= 18)
+                {
+                    ticketPrice = 15;
+                }
+                else if (age <= 64)
+                {
+                    ticketPrice = 20;
+                }
+                else
+                {
+                    ticketPrice = 15;
+                }
+            }
+            else
+            {
+                if (age <= 18)
+                {
+                    ticketPrice = 5;
+                }
+                else if (age <= 64)
+                {
+                    ticketPrice = 12;
+                }
+                else
+                {
+                    ticketPrice = 10;
+                }
+            }
+            if (age < 0 || age > 122)
+            {
+                Console.WriteLine("Error!");
+            }
+            else
+            {
+                Console.WriteLine($"{ticketPrice}$");
+            }
+        }
+    }
+}
---
+++ 01. Basic Syntax, Conditional Statements and Loops Lab/11. Multiplication Tabe 2.0.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _11._Multiplication_Tabe_2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int multiplier = int.Parse(Console.ReadLine());
+
+            for (int i = multiplier; i <= 10; i++)
+            {
+                Console.WriteLine($"{num} X {i} = {num * i}");
+            }
+
+            if (multiplier > 10)
+            {
+                Console.WriteLine($"{num} X {multiplier} = {num * multiplier}");
+            }
+        }
+    }
+}
