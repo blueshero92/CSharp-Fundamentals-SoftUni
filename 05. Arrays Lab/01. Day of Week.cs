c1374dc7fe7344b3a81022941edcaa0d6a8a686d
blueshero92 <133801686+blueshero92@users.noreply.github.com>
2023-06-02 01:18:17 +0300

    Add files via upload

---
+++ 05. Arrays Lab/01. Day of Week.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _1._Day_of_Week
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int day = int.Parse(Console.ReadLine());
+
+            string[] days = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+            
+                if (day < 1 || day > 7)
+                {
+                    Console.WriteLine("Invalid day!");
+                }
+                else
+                {
+                    Console.WriteLine(days[day - 1]);
+                }
+                                                  
+        }
+    }
+}
---
+++ 05. Arrays Lab/07. Equal Arrays.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace _07._Equal_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = Console
+                           .ReadLine()
+                           .Split(" ")
+                           .Select(int.Parse)
+                           .ToArray();
+
+            int[] array2 = Console
+                           .ReadLine()
+                           .Split(" ")
+                           .Select(int.Parse)
+                           .ToArray();
+           
+
+           for (int i = 0; i < array1.Length; i++)
+            {
+                
+
+                if (array1[i] != array2[i])
+                {
+                    Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                    Environment.Exit(0);
+                }                             
+                
+            }
+
+            Console.WriteLine($"Arrays are identical. Sum: {array1.Sum()}");
+
+        }
+    }
+}
