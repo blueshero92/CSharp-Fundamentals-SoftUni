a813a2c6dbf422a59e800cf56e36080c756fbda9
blueshero92 <133801686+blueshero92@users.noreply.github.com>
2023-06-02 01:26:08 +0300

    Add files via upload

---
+++ 07. Methods Lab/03. Calculations.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _03._Calculations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string calculation = Console.ReadLine();
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            if (calculation == "add")
+            {
+                Add(a, b);
+            }
+            else if (calculation == "subtract")
+            {
+                Subtract(a, b);
+            }
+            else if (calculation == "multiply")
+            {
+                Multiply(a, b);
+            }
+            else
+            {
+                Divide(a, b);
+            }
+        }
+
+        static void Add(int a, int b)
+        {
+            int result = a + b;
+            Console.WriteLine(result);
+        }
+        static void Subtract (int a, int b)
+        {
+            int result = a - b;
+            Console.WriteLine(result);
+        }
+        static void Multiply (int a, int b)
+        {
+            int result = a * b;
+            Console.WriteLine(result);
+        }
+        static void Divide (int a, int b)
+        {
+            int result =  a / b;
+            Console.WriteLine(result);
+        }
+
+
+    }
+}
---
+++ 07. Methods Lab/04. Printing Triangle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _04._Printing_Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            PrintingTriangle(n);
+        }
+
+        static void PrintingTriangle(int n)
+        {
+            PrintingTriangleTop(n);
+            PrintingTriangleBottom(n - 1);
+        }
+
+        private static void PrintingTriangleTop(int n)
+        {
+            for (int row = 1; row <= n; row++)
+            {
+                for (int col = 1; col <= row; col++)
+                {
+                    Console.Write(col + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        private static void PrintingTriangleBottom(int n)
+        {
+            for (int row = n; row >= 1; row--)
+            {
+                for (int col = 1; col <= row; col++)
+                {
+                    Console.Write(col + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
