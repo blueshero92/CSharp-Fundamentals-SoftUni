34aff0705a28b2bec3674c8afecdaced9d1b11e1
blueshero92 <133801686+blueshero92@users.noreply.github.com>
2023-06-23 09:04:15 +0300

    Add files via upload

---
+++ Objects and Classes Lab/01. Randomize Words.cs
@@ -0,0 +1,25 @@
+﻿namespace _01._Randomize_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split().ToArray();
+
+            Random random = new Random();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                int randomIndex = random.Next(0, input.Length);
+
+                string currentIndexValue = input[i];
+                string randomIndexValue = input[randomIndex];
+
+                input[i] = randomIndexValue;
+                input[randomIndex] = currentIndexValue;
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, input));
+        }
+    }
+}
---
+++ Objects and Classes Lab/04. Students.cs
@@ -0,0 +1,49 @@
+﻿using System.Security.Cryptography;
+
+namespace _04._Students
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+
+            List<Student> students = new List<Student>();
+
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] data = input.Split();
+
+                Student student = new Student();
+
+                student.FirstName = data[0];
+                student.LastName = data[1];
+                student.Age = int.Parse(data[2]);
+                student.HomeTown = data[3];
+
+                students.Add(student);
+            }
+
+            string cityNameToPrint = Console.ReadLine();
+
+            foreach (Student student in students)
+            {
+                if (cityNameToPrint == student.HomeTown)
+                {
+                    Console.WriteLine($"{student.FirstName} {student.LastName} is {student.Age} years old.");
+                }
+            }
+        }
+    }
+
+    class Student
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        public string HomeTown { get; set;}
+    }
+}
